47ff5d543904f27cd8e748a0e1fc11aa03878f0b
Tiffany Subin Kim <sbtiffanykim@gmail.com>
2024-09-23 15:33:40 +0900

    style: Edit box style

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -18,24 +18,53 @@ const Loader = styled.div`
 
 const List = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-  gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   width: 100%;
   box-sizing: border-box;
 `;
 
-const Card = styled.div<{ bgphoto: string }>`
-  height: 180px;
-  width: 280px;
-  background-image: url(${(props) => props.bgphoto});
-  background-position: center center;
-  background-size: cover;
+const Card = styled.div`
+  height: auto;
+  width: 300px;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 35px;
   transition: transform 0.2s ease;
   &:hover {
     transform: scale(1.1);
   }
 `;
+
+const Image = styled.div<{ bgphoto: string }>`
+  height: 180px;
+  width: 100%;
+  background-image: url(${(props) => props.bgphoto});
+  background-position: center center;
+  background-size: cover;
+`;
+
+const Info = styled.div`
+  background-color: ${(props) => props.theme.black.light};
+  padding: 2px 7px;
+  height: 40px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const MediaType = styled.span`
+  background-color: #0984e3;
+  color: ${(props) => props.theme.white.light};
+  padding: 3px 7px;
+  border-radius: 3px;
+  margin-right: 10px;
+  font-size: 11px;
+`;
+
+const Title = styled.span``;
 
 interface ISearchResult {
   page: number;
@@ -60,11 +89,14 @@ export default function Search() {
       ) : multi && multi.results.length > 1 ? (
         <List>
           {multi.results.map((item) => (
-            <Card
-              key={item.id}
-              bgphoto={makeImagePath(item.backdrop_path || item.poster_path, 'w500')}
-            >
-              {item.title}
+            <Card key={item.id}>
+              <Image
+                bgphoto={makeImagePath(item.backdrop_path || item.poster_path, 'w500')}
+              />
+              <Info>
+                <MediaType>{item.media_type.toUpperCase()}</MediaType>
+                <Title>{item.title || item.name}</Title>
+              </Info>
             </Card>
           ))}
         </List>
